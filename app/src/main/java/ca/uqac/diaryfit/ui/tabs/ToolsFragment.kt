84aad293d4b217e565e2ff46dd3a8f6577b8b37c
willi <william29042000@gmail.com>
2022-11-20 15:40:46 -0500

    Ajout chrono

--- app/src/main/java/ca/uqac/diaryfit/ui/tabs/ToolsFragment.kt
+++ app/src/main/java/ca/uqac/diaryfit/ui/tabs/ToolsFragment.kt
@@ -1,10 +1,15 @@
 package ca.uqac.diaryfit.ui.tabs
 
 import android.os.Bundle
+import android.os.SystemClock
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Chronometer
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import ca.uqac.diaryfit.R
 import ca.uqac.diaryfit.databinding.FragmentToolsBinding
 
 class ToolsFragment : Fragment() {
@@ -20,6 +25,54 @@ class ToolsFragment : Fragment() {
     ): View {
         _binding = FragmentToolsBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        val chrono = root.findViewById<Chronometer>(R.id.fg_tools_chrono)
+
+        var isWorking = false
+        var pauseOffset = 0
+
+        val start_btn = root.findViewById<Button>(R.id.fg_tools_btn_start)
+        val reset_btn = root.findViewById<Button>(R.id.fg_tools_btn_reset)
+
+        start_btn?.setOnClickListener(object : View.OnClickListener {
+
+            override fun onClick(v: View) {
+                if (!isWorking) {
+                    chrono.base = SystemClock.elapsedRealtime() - pauseOffset
+                    chrono.start()
+                    isWorking = true
+                } else {
+                    chrono.stop()
+                    pauseOffset = (SystemClock.elapsedRealtime() - chrono.base).toInt()
+                    isWorking = false
+                }
+
+                start_btn.setText(if (isWorking) R.string.stop else R.string.start)
+
+                Toast.makeText(requireActivity(), getString(
+                        if (isWorking)
+                            R.string.working
+                        else
+                            R.string.stopped
+                    ), Toast.LENGTH_SHORT).show()
+            }
+        })
+
+
+        reset_btn?.setOnClickListener(object : View.OnClickListener{
+
+            override fun onClick(p0: View?) {
+                pauseOffset = 0
+                chrono.base = SystemClock.elapsedRealtime()
+                chrono.stop()
+                isWorking = false
+
+                start_btn.setText(if (isWorking) R.string.stop else R.string.start)
+
+                Toast.makeText(requireActivity(),"Time reset",Toast.LENGTH_SHORT).show()
+            }
+        })
+
 
 
         return root
