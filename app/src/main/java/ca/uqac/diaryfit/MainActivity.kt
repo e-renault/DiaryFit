977bc2ec0803c098d745a5efedfe849917ba792a
Erwan <erw.renault@gmail.com>
2022-12-14 18:15:06 -0500

    Minor fixes

--- app/src/main/java/ca/uqac/diaryfit/MainActivity.kt
+++ app/src/main/java/ca/uqac/diaryfit/MainActivity.kt
@@ -69,7 +69,7 @@ class MainActivity : AppCompatActivity() {
                         // menu should be considered as top level destinations.
                         val appBarConfiguration = AppBarConfiguration(
                             setOf(
-                                R.id.navigation_main, R.id.navigation_calendar, R.id.navigation_tools, R.id.navigation_stats, R.id.navigation_measures
+                                R.id.navigation_main, R.id.navigation_calendar, R.id.navigation_tools, R.id.navigation_stats
                             )
                         )
                         setupActionBarWithNavController(navController, appBarConfiguration)
